9ed4c7e08f18aa8cb1fd429bbb7f8d41933c9b2d
Peter Behringer <peterbehringer@gmx.de>
2015-03-11 15:15:27 -0400

    latest changes

--- simpleITK_EulerRotation.py
+++ simpleITK_EulerRotation.py
@@ -32,7 +32,7 @@ eulerTransPathAfterRotation = '/Users/peterbehringer/MyTesting/SimpleITK_Tests/e
 rotatedImage='/Users/peterbehringer/MyTesting/SimpleITK_Tests/Rotated_image_1.nrrd'
 bestEulerTransPath='/Users/peterbehringer/MyTesting/SimpleITK_Tests/bestEulerTrans.h5'
 
-# INITIALIZATION - ROTATION - METRIC MEASURE
+# INITIALIZATION
 # _______________________________
 
 # Initialize ImageRegistrationMethod()
@@ -56,37 +56,14 @@ Reg.SetInitialTransform(eulerTrans)
 # write the transform
 sitk.WriteTransform(eulerTrans,eulerTransPath)
 
-print ('>> Step 1: after centered Initialization >> Metricvalue : '+str(Reg.MetricEvaluate(fixedVolume,movingVolume)))
 
-# print out center
-# center=eulerTrans.GetCenter()
-# print ('Rotation Center : '+str(center))
-
-# set Rotation
-eulerTrans.SetRotation(0,0,45)
-# print ('Parameters eulerTrans : '+str(eulerTrans.GetParameters()))
-
-# Update Transform
-Reg.SetInitialTransform(eulerTrans)
-
-# write the transform
-sitk.WriteTransform(eulerTrans,eulerTransPathAfterRotation)
-
-print ('>> Step 2: after 45 Rotation axial >> Metricvalue : '+str(Reg.MetricEvaluate(fixedVolume,movingVolume)))
-
-# set Rotation
-eulerTrans.SetRotation(23,0,0)
-# print ('Parameters eulerTrans : '+str(eulerTrans.GetParameters()))
-
-# Update Transform
-Reg.SetInitialTransform(eulerTrans)
-
-print ('>> Step 3: after 23 Rotation axial >> Metricvalue : '+str(Reg.MetricEvaluate(fixedVolume,movingVolume)))
+# ROTATE & MEASURE METRIC
+# _______________________________
 
 one_degree=1.0*pi/180.0
 
-axAngleRange = 2.0
-sagAngleRange = 2.0
+axAngleRange = 12.0
+sagAngleRange = 12.0
 
 axStepSize = 3.0 * one_degree
 sagStepSize = 3.0 * one_degree
@@ -97,24 +74,8 @@ initialMetricValue= Reg.MetricEvaluate(fixedVolume,movingVolume)
 # initialize output Transform with Translation from CenteredTransformInitializer
 bestEulerTrans=sitk.Euler3DTransform(sitk.CenteredTransformInitializer(fixedMask,movingMask,sitk.Euler3DTransform()))
 
-
-for axAngle in np.arange(0.0,axAngleRange,axStepSize):
-
-
-        eulerTrans.SetRotation(0,0,axAngle)
-        Reg.SetInitialTransform(eulerTrans)
-        currentMetricValue = Reg.MetricEvaluate(fixedVolume,movingVolume)
-
-        if abs(currentMetricValue) < abs(initialMetricValue):
-            bestEulerTrans.SetRotation(0,0,axAngle)
-            # print ('new best Euler Trans found at axAngle = '+str(axAngle))
-
-sitk.WriteTransform(bestEulerTrans,bestEulerTransPath)
-
-"""
-for axAngle in np.arange(0.0,axAngleRange,axStepSize):
-    for sagAngle in np.arange(0.0,sagAngleRange,sagStepSize):
-
+for axAngle in np.arange(-axAngleRange,axAngleRange,axStepSize):
+    for sagAngle in np.arange(-sagAngleRange,sagAngleRange,sagStepSize):
 
         eulerTrans.SetRotation(sagAngle,0,axAngle)
         Reg.SetInitialTransform(eulerTrans)
@@ -125,4 +86,4 @@ for axAngle in np.arange(0.0,axAngleRange,axStepSize):
             print ('new best Euler Trans found at sagAngle = '+str(sagAngle)+' and axAngle = '+str(axAngle))
 
 sitk.WriteTransform(bestEulerTrans,bestEulerTransPath)
-"""
+
