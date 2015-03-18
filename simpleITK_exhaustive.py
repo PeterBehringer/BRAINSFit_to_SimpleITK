ff6dc09fc1f1df260b4a73eaf6ad9e8a814de142
Peter Behringer <peterbehringer@gmx.de>
2015-03-18 16:24:10 -0400

    Added Exhaustive Optimizer

--- simpleITK_exhaustive.py
+++ simpleITK_exhaustive.py
@@ -5,6 +5,14 @@ from math import pi
 import numpy as np
 
 
+def command_iteration(method) :
+    if (method.GetOptimizerIteration()==0):
+        print("Scales: ", method.GetOptimizerScales())
+    print("{0:3} = {1:7.5f} : {2}".format(method.GetOptimizerIteration(),
+                                           method.GetMetricValue(),
+                                           method.GetOptimizerPosition()))
+
+
 # read input volumes
 
 fixedImageFilename = '/Users/peterbehringer/MyImageData/ProstateRegistrationValidation/Images/Case1-t2ax-intraop.nrrd'
@@ -23,32 +31,31 @@ movingMask=sitk.ReadImage(movingMaskFilename, sitk.sitkFloat32)
 
 # set output file paths
 
-outputTransform = '/Users/peterbehringer/MyTesting/SimpleITK_Tests/OutTrans_Rigid_1.h5'
-outputVolume = '/Users/peterbehringer/MyTesting/SimpleITK_Tests/OutVol_Rigid_1.nrrd'
-outputTransform_Initializer = '/Users/peterbehringer/MyTesting/SimpleITK_Tests/OutTrans_Initializer_Rigid__1.h5'
-ctx1Data = '/Users/peterbehringer/MyTesting/SimpleITK_Tests/ctx1.h5'
-ctx2Data = '/Users/peterbehringer/MyTesting/SimpleITK_Tests/ctx2.h5'
-eulerTransPath = '/Users/peterbehringer/MyTesting/SimpleITK_Tests/eulerTrans.h5'
+
+eulerTransPath = '/Users/peterbehringer/MyTesting/SimpleITK_Tests/initial_exhaustive.h5'
 eulerTransPathAfterRotation = '/Users/peterbehringer/MyTesting/SimpleITK_Tests/eulerTransAfterRotation.h5'
 rotatedImage='/Users/peterbehringer/MyTesting/SimpleITK_Tests/Rotated_image_1.nrrd'
 bestEulerTransPath='/Users/peterbehringer/MyTesting/SimpleITK_Tests/bestEulerTrans.h5'
-outTxPath='/Users/peterbehringer/MyTesting/SimpleITK_Tests/outTx_LATEST.h5'
-quickSetVersorInitial='/Users/peterbehringer/MyTesting/SimpleITK_Tests/quickVersorInitial.h5'
+outTxPath='/Users/peterbehringer/MyTesting/SimpleITK_Tests/outTx_exhaustive.h5'
+trans_rigid_beforeRegistrationPath='/Users/peterbehringer/MyTesting/SimpleITK_Tests/trans_rigid_beforeRegistration.h5'
+outTxAfterRigidPath='/Users/peterbehringer/MyTesting/SimpleITK_Tests/outTxAfterRigid.h5'
 
 
-# INITIALIZATION
+
+# INITIALIZATION (Translation & Rotation)
 # _______________________________
 
 # Initialize ImageRegistrationMethod()
 Reg=sitk.ImageRegistrationMethod()
 Reg.SetMetricFixedMask(fixedMask)
 Reg.SetMetricMovingMask(movingMask)
-Reg.SetMetricAsCorrelation()
+Reg.SetMetricAsMattesMutualInformation(numberOfHistogramBins = 50)
 Reg.SetInterpolator(sitk.sitkLinear)
-Reg.SetOptimizerAsRegularStepGradientDescent(learningRate=2.0,
-                                          minStep=1e-4,
-                                          numberOfIterations=1,
-                                          gradientMagnitudeTolerance=1e-8 )
+
+# use exhaustive Optimizer
+sample_per_axis=12
+Reg.SetOptimizerAsExhaustive([sample_per_axis/2,sample_per_axis/2,sample_per_axis/4,0,0,0])
+Reg.SetOptimizerScales([2.0*pi/sample_per_axis,2.0*pi/sample_per_axis,2.0*pi/sample_per_axis,1.0,1.0,1.0])
 
 # Set the Euler3DTransform
 eulerTrans=sitk.Euler3DTransform(sitk.CenteredTransformInitializer(fixedMask,movingMask,sitk.Euler3DTransform()))
@@ -57,53 +64,27 @@ Reg.SetInitialTransform(eulerTrans)
 # write the transform
 sitk.WriteTransform(eulerTrans,eulerTransPath)
 
+# Execute & write
+Reg.AddCommand(sitk.sitkIterationEvent, lambda: command_iteration(Reg))
+outTx = Reg.Execute(fixedVolume, movingVolume)
+sitk.WriteTransform(outTx,outTxPath)
 
-# ROTATE & MEASURE METRIC
-# like here https://github.com/BRAINSia/BRAINSTools/blob/19fa37dfbdee37deff4ccee412bb601f7a787bda/BRAINSCommonLib/BRAINSFitHelperTemplate.hxx#L325-L339
-# _______________________________
-
-one_degree=1.0*pi/180.0
-
-axAngleRange = 1.0
-# sagAngleRange = 12.0
-
-axStepSize = 3.0 * one_degree
-# sagStepSize = 3.0 * one_degree
-
-# set current Metric value
-initialMetricValue= Reg.MetricEvaluate(fixedVolume,movingVolume)
-
-# initialize output Transform with Translation from CenteredTransformInitializer
-bestEulerTrans=sitk.Euler3DTransform(sitk.CenteredTransformInitializer(fixedMask,movingMask,sitk.Euler3DTransform()))
-
-for axAngle in np.arange(-axAngleRange,axAngleRange,axStepSize):
-    #for sagAngle in np.arange(-sagAngleRange,sagAngleRange,sagStepSize):
-
-        eulerTrans.SetRotation(0,0,axAngle)
-        print ('current axAngle : '+str(axAngle))
-        Reg.SetInitialTransform(eulerTrans)
-        currentMetricValue = Reg.MetricEvaluate(fixedVolume,movingVolume)
-
-        if currentMetricValue < initialMetricValue:
-            print ('new best axAngle : '+str(axAngle))
-            bestEulerTrans.SetRotation(0,0,axAngle)
-            # print ('new best Euler Trans found at sagAngle = '+str(sagAngle)+' and axAngle = '+str(axAngle))
-
-sitk.WriteTransform(bestEulerTrans,bestEulerTransPath)
 
 # RIGID REGISTRATION PHASE
 # _______________________________
 
-quickSetVersor=sitk.VersorRigid3DTransform()
-quickSetVersor.SetCenter(bestEulerTrans.GetCenter())
-quickSetVersor.SetTranslation(bestEulerTrans.GetTranslation())
-quickSetVersor.SetMatrix(bestEulerTrans.GetMatrix())
+# trans_afterInit=sitk.ReadTransform('/Users/peterbehringer/MyTesting/SimpleITK_Tests/outTx_exhaustive.h5')
 
-sitk.WriteTransform(quickSetVersor,quickSetVersorInitial)
+quickSetVersor=sitk.VersorRigid3DTransform()
+quickSetVersor.SetCenter(outTx.GetCenter())
+quickSetVersor.SetTranslation(outTx.GetTranslation())
+quickSetVersor.SetMatrix(outTx.GetMatrix())
+
+sitk.WriteTransform(quickSetVersor,trans_rigid_beforeRegistrationPath)
 
 Reg2=sitk.ImageRegistrationMethod()
 Reg2.SetInitialTransform(quickSetVersor)
-Reg2.SetMetricAsMattesMutualInformation(50)
+Reg2.SetMetricAsMattesMutualInformation(numberOfHistogramBins = 50)
 Reg2.SetMetricFixedMask(fixedMask)
 Reg2.SetMetricMovingMask(movingMask)
 Reg2.SetInterpolator(sitk.sitkLinear)
@@ -129,14 +110,23 @@ Reg2.SetInterpolator(sitk.sitkLinear)
 # EstimateLearningRateType estimateLearningRate=Never     ?
 # double maximumStepSizeInPhysicalUnits                   0.2
 
-Reg2.SetOptimizerAsRegularStepGradientDescent(learningRate=2.0,
-                                          minStep=0.005,
+Reg2.SetOptimizerAsRegularStepGradientDescent(minStep=0.005,
                                           numberOfIterations=100,
                                           relaxationFactor = 0.5,
                                           gradientMagnitudeTolerance=1e-5,
                                           maximumStepSizeInPhysicalUnits=0.2)
 
 
-outTx = Reg2.Execute(fixedVolume, movingVolume)
+outTxAfterRigid = Reg2.Execute(fixedVolume, movingVolume)
 
-sitk.WriteTransform(outTx,outTxPath)
+sitk.WriteTransform(outTxAfterRigid,outTxAfterRigidPath)
+
+
+# AFFINE REGISTRATION PHASE
+# _______________________________
+
+
+
+
+# BSPLINE REGISTRATION PHASE
+# _______________________________
